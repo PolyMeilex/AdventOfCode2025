f3c0a0129e2dfc3758906bdca26674395b3ceb57
PolyMeilex <marynczakbartlomiej@gmail.com>
2025-12-01 21:59:33 +0100

    rust: day1

---
+++ src/day1/mod.rs
@@ -0,0 +1,110 @@
+#[derive(Debug)]
+struct Lock {
+    value: u32,
+}
+
+impl Default for Lock {
+    fn default() -> Self {
+        Self { value: 50 }
+    }
+}
+
+// 99, 0, 1
+//  ┌─────┐
+//  │     │
+//  │     │
+//  └─────┘
+//
+
+impl Lock {
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn left(&mut self, by: u32) {
+        let new_value = self.value as i32 - by as i32;
+
+        if new_value.is_negative() {
+            self.value = 100 - new_value.unsigned_abs();
+        } else {
+            self.value = new_value as u32;
+        }
+    }
+
+    fn right(&mut self, by: u32) {
+        self.value = (self.value + by) % 100;
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Direction {
+    Left,
+    Right,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Command {
+    value: u32,
+    direction: Direction,
+}
+
+impl Command {
+    fn parse_lines(src: &str) -> Vec<Self> {
+        src.lines()
+            .map(|line| line.trim())
+            .filter(|line| !line.is_empty())
+            .map(Self::parse)
+            .collect()
+    }
+
+    fn parse(line: &str) -> Self {
+        let (direction, value) = line.split_at(1);
+
+        let direction = match direction {
+            "L" => Direction::Left,
+            "R" => Direction::Right,
+            _ => unreachable!(),
+        };
+
+        let value: u32 = value.parse().unwrap();
+
+        Self { value, direction }
+    }
+}
+
+pub fn run(part2: bool) {
+    let src = include_str!("./input.txt");
+
+    let commands = Command::parse_lines(src.trim());
+
+    let mut lock = Lock::new();
+
+    let mut count = 0;
+    for command in commands {
+        if part2 {
+            for _ in 0..command.value {
+                match command.direction {
+                    Direction::Left => lock.left(1),
+                    Direction::Right => lock.right(1),
+                }
+
+                if lock.value == 0 {
+                    count += 1;
+                }
+            }
+        } else {
+            for _ in 0..command.value {
+                match command.direction {
+                    Direction::Left => lock.left(1),
+                    Direction::Right => lock.right(1),
+                }
+            }
+
+            if lock.value == 0 {
+                count += 1;
+            }
+        }
+    }
+
+    dbg!(count);
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod day1;
+
+fn main() {
+    day1::run(false);
+    day1::run(true);
+}
