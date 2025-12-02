afa222fd3f4992692d6dacc46082dfa79273adf7
PolyMeilex <marynczakbartlomiej@gmail.com>
2025-12-02 21:01:15 +0100

    rust: Day2 part2

--- rust/src/day2/mod.rs
+++ rust/src/day2/mod.rs
@@ -1,6 +1,6 @@
 use std::ops::RangeInclusive;
 
-pub fn run() {
+pub fn run(part2: bool) {
     let src = include_str!("./input2.txt").trim();
     let ranges: Vec<_> = src
         .split(",")
@@ -12,7 +12,11 @@ pub fn run() {
 
     let mut out = 0;
     for range in ranges {
-        check_range(range, &mut out);
+        if part2 {
+            check_range_part2(range, &mut out);
+        } else {
+            check_range(range, &mut out);
+        }
     }
 
     dbg!(out);
@@ -25,6 +29,28 @@ fn check_range(range: RangeInclusive<u64>, out: &mut u64) {
         let (a, b) = v.split_at(v.len() / 2);
 
         if a == b {
+            *out += i;
+        }
+    }
+}
+
+// 12345 12345             : Source
+// 12345 12345 12345 12345 : Double Source
+//  2345 12345 12345 1234  : Trim start-end to avoid false positives
+//       XXXXXXXXXXX       : Matched source
+//
+// 12 12 12 12             : Source
+// 12 12 12 12 12 12 12 12 : Double Source
+//  2 12 12 12 12 12 12 1  : Trim start-end to avoid false positives
+//    XXXXXXXXXXX          : Matched source
+fn check_range_part2(range: RangeInclusive<u64>, out: &mut u64) {
+    for i in range {
+        let chars = i.to_string();
+
+        let work_area = format!("{chars}{chars}");
+        let trimmed = work_area.get(1..work_area.len() - 1).unwrap();
+
+        if trimmed.contains(&chars) {
             *out += i;
         }
     }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,5 +4,6 @@ mod day2;
 fn main() {
     day1::run(false);
     day1::run(true);
-    day2::run();
+    day2::run(false);
+    day2::run(true);
 }
