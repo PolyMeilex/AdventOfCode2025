ca7886f6f0c702b061657bbe83c3e427d9896173
PolyMeilex <marynczakbartlomiej@gmail.com>
2025-12-06 20:46:21 +0100

    rust: Day6 part2

--- rust/src/day6/mod.rs
+++ rust/src/day6/mod.rs
@@ -1,70 +1,96 @@
-enum LexerState {
-    InNum,
-    InSpace,
-}
+use std::ops::RangeInclusive;
 
+#[derive(Debug)]
 struct Task {
     ops: char,
     values: Vec<u64>,
 }
 
 impl Task {
-    fn new(ops: &[String], numbers: &[Vec<String>], task_idx: usize) -> Self {
-        let ops = ops[task_idx].chars().next().unwrap();
+    fn new(ops: &str, numbers: &[&str]) -> Self {
+        let ops = ops.chars().next().unwrap();
         let values = numbers
             .iter()
-            .map(|row| row[task_idx].parse::<u64>().unwrap())
+            .map(|row| row.trim().parse::<u64>().unwrap())
             .collect();
         Self { ops, values }
     }
+
+    fn new_part2(ops: &str, numbers: &[&str]) -> Self {
+        let ops = ops.chars().next().unwrap();
+
+        let width = numbers.iter().map(|v| v.len()).max().unwrap();
+
+        let mut out = Vec::new();
+        for col in 0..width {
+            let mut digits = String::new();
+            for row in numbers.iter() {
+                if let Some(digit) = &row.get(col..=col) {
+                    digits += digit;
+                }
+            }
+            out.push(digits.trim().parse::<u64>().unwrap());
+        }
+
+        Self { ops, values: out }
+    }
 }
 
-pub fn run(_part2: bool) {
-    let src = include_str!("./input2.txt").trim();
+type ColumnSpan = RangeInclusive<usize>;
+
+pub fn calc_column_spans(ops_row: &str) -> Vec<ColumnSpan> {
+    let mut out = Vec::<RangeInclusive<usize>>::new();
+
+    for (idx, ch) in ops_row.char_indices() {
+        if ch.is_whitespace() {
+            let current = out.last_mut().unwrap();
+            *current = *current.start()..=idx;
+        } else {
+            out.push(idx..=idx);
+        }
+    }
+
+    // Strip empty columns, last one does not have one
+    let count = out.len() - 1;
+    for range in out.iter_mut().take(count) {
+        let start = *range.start();
+        let end = range.end() - 1;
+        *range = start..=end;
+    }
+
+    out
+}
+
+pub fn slice_columns<'a>(column_spans: &[ColumnSpan], rows: &[&'a str]) -> Vec<Vec<&'a str>> {
+    column_spans
+        .iter()
+        .map(|span| rows.iter().map(|row| &row[span.clone()]))
+        .map(|iter| iter.collect())
+        .collect()
+}
+
+pub fn run(part2: bool) {
+    let src = include_str!("./input2.txt");
 
     let lines: Vec<&str> = src.lines().collect();
 
-    let mut processed_lines = Vec::new();
-    let mut state = LexerState::InSpace;
-    for line in lines {
-        let line = line.trim();
+    let column_spans = calc_column_spans(lines[lines.len() - 1]);
 
-        let mut out = Vec::new();
-        let mut staging = String::new();
-        for ch in line.chars() {
-            match state {
-                LexerState::InNum => {
-                    if ch.is_whitespace() {
-                        state = LexerState::InSpace;
-                        out.push(std::mem::take(&mut staging));
-                    } else {
-                        staging.push(ch);
-                    }
-                }
-                LexerState::InSpace => {
-                    if !ch.is_whitespace() {
-                        state = LexerState::InNum;
-                        staging.push(ch);
-                    }
-                }
-            }
-        }
+    let ops: Vec<_> = slice_columns(&column_spans, &lines[lines.len() - 1..])
+        .into_iter()
+        .map(|v| v[0])
+        .collect();
+    let numbers = slice_columns(&column_spans, &lines[..lines.len() - 1]);
 
-        out.push(std::mem::take(&mut staging));
-
-        processed_lines.push(out);
-    }
-
-    let tasks_count = processed_lines[0].len();
-    let tasks_height = processed_lines.len() - 1;
-    let ops_idx = tasks_height;
-
-    let numbers = &processed_lines[0..tasks_height];
-    let ops = &processed_lines[ops_idx];
+    let tasks_count = column_spans.len();
 
     let mut out = 0;
     for task_idx in 0..tasks_count {
-        let task = Task::new(ops, numbers, task_idx);
+        let task = if part2 {
+            Task::new_part2(ops[task_idx], &numbers[task_idx])
+        } else {
+            Task::new(ops[task_idx], &numbers[task_idx])
+        };
 
         let res = match task.ops {
             '*' => task.values.iter().product::<u64>(),
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -8,13 +8,19 @@ mod day6;
 fn main() {
     day1::run(false);
     day1::run(true);
+
     day2::run(false);
     day2::run(true);
+
     day3::run(false);
     day3::run(true);
+
     day4::run(false);
     day4::run(true);
+
     day5::run(false);
     day5::run(true);
+
     day6::run(false);
+    day6::run(true);
 }
